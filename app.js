6eb7db1d53d9444464b65bfe9266264c916bd85a
Jan Szczęśniewicz <janszczesniewicz@icloud.com>
2020-05-06 01:00:34 +0200

    upss

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const photos = require('./routes/photos');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-let corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-app.use(cors(corsOptions));
+// let corsOptions = {
+//     origin: '*',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+app.use(cors);
 
 //Routes
 app.use('/photos', photos);
